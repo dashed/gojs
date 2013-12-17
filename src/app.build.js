7bc33ea18d9c137b12f3521fd93508119ba603c7
Alberto <mail4alberto@gmail.com>
2013-12-17 09:01:34 -0500

    Last commit  :(

--- src/app.build.js
+++ src/app.build.js
@@ -1,44 +1,10 @@
 ({
     mainConfigFile: "main.js",
-    //exclude: ['main'],
-    /*skipDirOptimize: true,
-    wrap: true,
-    optimize: "uglify2",
-    appDir: "../",
-    baseUrl: "src",
-    dir: "../build",
-    paths: {
-        //jquery: "empty:"
-        requireLib: 'require'
-    },
-
-    // see: http://requirejs.org/docs/faq-advanced.html
-    modules: [
-        {
-            name: "gojs",
-            include: ["requireLib", "main"],
-            create: true
-        }
-    ]*/
-
-    // baseUrl: ".",
-    // optimize: "uglify2",
-    // wrap: true,
-    // paths: {
-    //     //jquery: "empty:"
-    //     requireLib: 'require'
-    // },
-    // name: "gojs",
-    // include: ["requireLib", "main"],
-    // create: true,
-    // out: "../dist/gojs_old_optimized.js"
-
     optimize: "uglify2",
     baseUrl: '.',
     name: './almond',
     include: ['main'],
     insertRequire: ['main'],
     out: "../dist/gojs.js",
-    wrap: true
-
+    wrap: true,
 })
