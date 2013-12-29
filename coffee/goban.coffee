60eb21643b7ccfce6cf765a68aaed63bfc5aa997
Alberto Leal <mail4alberto@gmail.com>
2013-12-29 01:55:32 -0500

    Create wrapper for Goban omitting the requirement for the new keyword.

---
+++ coffee/goban.coffee
@@ -0,0 +1,14 @@
+define ["main", "lodash"], (Goban, _) ->
+    ###
+    Goban(...) instead of new Goban(...)
+    But either is possible.
+    ###
+    factory = (length, width) ->
+        return new Goban(length, width)
+
+    # Emulate properties
+    _.each(_.keys(Goban), (key)->
+        factory[key] = Goban[key]
+        )
+
+    return factory
---
+++ src/goban.js
@@ -0,0 +1,15 @@
+define(["main", "lodash"], function(Goban, _) {
+  /*
+  Goban(...) instead of new Goban(...)
+  But either is possible.
+  */
+
+  var factory;
+  factory = function(length, width) {
+    return new Goban(length, width);
+  };
+  _.each(_.keys(Goban), function(key) {
+    return factory[key] = Goban[key];
+  });
+  return factory;
+});
