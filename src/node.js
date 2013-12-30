30d06c438686ab7d1669f3c840f6fbbfa9022010
Alberto Leal <mail4alberto@gmail.com>
2013-12-30 02:29:33 -0500

    Clean up

--- src/node.js
+++
@@ -1,9 +0,0 @@
-var requirejs;
-
-requirejs = require('requirejs');
-
-requirejs(['config', 'goban', 'lodash'], function(config, Goban, _) {
-  var lol;
-  lol = Goban();
-  return console.log(lol.config);
-});
